5b4cc131c09de84a39c33468c8f0de2211a3de2d
Raphael Brown <89517683+RaphaelMilesBrown@users.noreply.github.com>
2023-09-26 16:28:31 -0400

    Update Knockback_MovementBehaviour.cs

--- WhenRobotsBleed-main/WhenRobotsBleed/Assets/RaphaelFolder/InternalAssets/Scripts/Behaviours/Knockback_MovementBehaviour.cs
+++ WhenRobotsBleed-main/WhenRobotsBleed/Assets/RaphaelFolder/InternalAssets/Scripts/Behaviours/Knockback_MovementBehaviour.cs
@@ -1,1 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Knockback_MovementBehaviour : MovementBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private int _kbForce; //350
+    //private float _kbCounter;
+    [SerializeField]
+    private float _kbDur; //0.02f
+    private bool _knockFromRight;
+
+    public IEnumerator Knockback(float knockDur, float knockbackPwr, Vector2 knockbackDir){
+      float timer = 0;
+      while(knockDur > timer){
+          timer+=Time.deltaTime;
+          rb.AddForce(new Vector2(knockbackDir.x * -100, knockbackDir.y * knockbackPwr));
+      }
+      yield return 0;
+    }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void OnEnable()
+    {
+        StartCoroutine(Knockback(_kbDur, _kbForce, transform.position));
+    }
+
+    void Update()
+    {
+        
+    }
+    
+}
