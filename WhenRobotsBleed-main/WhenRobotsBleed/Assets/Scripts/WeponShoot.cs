2343160e5c044144995196f3f43f90e02b18b8f0
RaphaelMilesBrown <rbrown48@gmu.edu>
2023-10-29 23:00:41 -0400

    Fixed shoot mechanic
    When you shoot, your shoot animation now plays. Also, the fire rate is capped.

--- WhenRobotsBleed-main/WhenRobotsBleed/Assets/Scripts/WeponShoot.cs
+++ WhenRobotsBleed-main/WhenRobotsBleed/Assets/Scripts/WeponShoot.cs
@@ -7,24 +7,40 @@ public class WeponShoot : MonoBehaviour
 	public Transform firePoint;
 	public GameObject bulletPrefab;
 	public bool hasGun = false;
-	
+	private Animator _playerAnimator;
+	private bool _isShooting = false;
+
+	void Awake(){
+		_playerAnimator = GetComponent<Animator>();
+	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		//Debug.Log(_playerAnimator.GetCurrentAnimatorStateInfo(0).shortNameHash);
 		if (hasGun)
         {
-			if (Input.GetButtonDown("Fire1"))
+			if (Input.GetButtonDown("Fire1") && _isShooting == false)
 			{
-				Shoot();
+				var currentState = _playerAnimator.GetCurrentAnimatorStateInfo(0).shortNameHash;
+				_isShooting = true;
+				//_playerAnimator.GetCurrentAnimatorStateInfo(0).shortNameHash;
+				StartCoroutine(Shoot(currentState));
+				Debug.Log("outside shoot");
+				
 			}
 		}
 		
 	}
 
 
-	void Shoot()
+	private IEnumerator Shoot(int intendedState)
 	{
+		Debug.Log("inside shoot");
+		_playerAnimator.Play("Character_Shoot");
 		Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		yield return new WaitForSeconds(0.4f);
+		_playerAnimator.Play(intendedState);
+		_isShooting = false;
 	}
 }
