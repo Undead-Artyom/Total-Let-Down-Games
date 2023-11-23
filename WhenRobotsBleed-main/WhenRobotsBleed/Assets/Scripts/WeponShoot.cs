217348f6bba8095ba2774446af6e049cdf9e0226
RaphaelMilesBrown <rbrown48@gmu.edu>
2023-11-22 22:59:03 -0500

    Update WeponShoot
    WeponShoot updated

--- WhenRobotsBleed-main/WhenRobotsBleed/Assets/Scripts/WeponShoot.cs
+++ WhenRobotsBleed-main/WhenRobotsBleed/Assets/Scripts/WeponShoot.cs
@@ -9,32 +9,46 @@ public class WeponShoot : MonoBehaviour
 	public bool hasGun = false;
 	private Animator _playerAnimator;
 	private bool _isShooting = false;
+	private PlayerAtack _playerAtack;
+	private bool _canShoot = true;
+	public bool canShoot => _canShoot;
+
+	private float _shootTime = 0.4f;
+	public float shootTime => _shootTime;
 
 	void Awake(){
 		_playerAnimator = GetComponent<Animator>();
+		_playerAtack = GetComponent<PlayerAtack>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (hasGun)
+		if (hasGun && _canShoot)
         {
 			if (Input.GetButtonDown("Fire1") && _isShooting == false)
 			{
 				var currentState = _playerAnimator.GetCurrentAnimatorStateInfo(0).shortNameHash;
 				_isShooting = true;
+				_playerAtack.ChangeCanAttack(false);
 				StartCoroutine(Shoot(currentState));
 			}
 		}
 	}
 
+	public void ChangeCanShoot(bool val){
+        if(val == true || val == false){
+            _canShoot = val;
+        }
+    }
 
 	private IEnumerator Shoot(int intendedState)
 	{
 		_playerAnimator.Play("Character_Shoot");
 		Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-		yield return new WaitForSeconds(0.4f);
+		yield return new WaitForSeconds(_shootTime);
 		_playerAnimator.Play(intendedState);
 		_isShooting = false;
+		_playerAtack.ChangeCanAttack(true);
 	}
 }
